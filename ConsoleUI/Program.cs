01f53a69196531148048dd9af4694566932f0506
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 14:59:34 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main()
 		{
 			for ( int i = 1; i < 50; i *= 5 )
 			{
